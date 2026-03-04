ad850029a0a9a21f3562e82e30c8c15471b5bb9e
Aubin SOSSOU <aubinsossoua@gmail.com>
2026-03-04 01:23:57 +0100

    finaly interface admin dashboard

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import '@/assets/css/global.css'
+import '@/assets/css/main.css'
 
 import App from './App.vue'
 import router from './router'
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/admin/adminDashboard.vue'
+import MatiereView from '../views/admin/matiere/ajoutMatiere.vue'
+import EnseignantDashboard from '../views/enseignant/enseignantDashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,19 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+       meta: { role: 'ecole' }
+    },
+    {
+      path: '/matiere',
+      name: 'matiere',
+      component: MatiereView,
+       meta: { role: 'ecole' }
+    },
+     {
+      path: '/enseignant/dashboard',
+      name: 'enseignantDashboard',
+      component: EnseignantDashboard,
+       meta: { role: 'enseignant' }
     },
     
   ],
